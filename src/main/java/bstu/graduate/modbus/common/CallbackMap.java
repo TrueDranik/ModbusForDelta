44909795324f89957a90cdb00fdd87a67df123c5
Ruslan N <>
2023-03-02 23:17:59 +0300

    [MOD-2] added a class for handling callbacks

---
+++ src/main/java/bstu/graduate/modbus/common/CallbackMap.java
@@ -0,0 +1,25 @@
+package bstu.graduate.modbus.common;
+
+import bstu.graduate.modbus.service.callbackquery.Callback;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CallbackMap {
+    private static final Map<String, Callback> CALLBACK_MAP = new HashMap<>();
+
+    public CallbackMap(List<Callback> callbacks) {
+        for (Callback callback : callbacks) {
+            if (callback != null) {
+                CALLBACK_MAP.put(callback.getActionCallback().toString(), callback);
+            }
+        }
+    }
+
+    public Callback getCallback(String keyCallback) {
+        return CALLBACK_MAP.get(keyCallback);
+    }
+}
