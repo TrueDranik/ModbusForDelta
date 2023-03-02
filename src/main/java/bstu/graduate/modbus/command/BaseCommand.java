e6b77f5a84d7a8d79b37510303265f682645cbcd
Ruslan N <>
2023-03-02 23:52:45 +0300

    [MOD-2] added a command handler

---
+++ src/main/java/bstu/graduate/modbus/command/BaseCommand.java
@@ -0,0 +1,12 @@
+package bstu.graduate.modbus.command;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+
+@Component
+public interface BaseCommand {
+    BotCommand getBotCommand();
+
+    BotApiMethod<?> getAction(Long chatId);
+}
