5e2e6f2a651865ab6e0ae9ba210dac7a7deca4d8
Ruslan N <>
2023-03-02 23:46:03 +0300

    [MOD-2] added enum with callbacks

---
+++ src/main/java/bstu/graduate/modbus/common/enums/CallbackEnum.java
@@ -0,0 +1,23 @@
+package bstu.graduate.modbus.common.enums;
+
+public enum CallbackEnum {
+    START_MENU,
+
+    /**
+     * Callbacks управления реле
+     */
+    RELAY_CONTROL_MENU,
+    TURN_ON_FIRST_RELAY,
+    TURN_OFF_FIRST_RELAY,
+
+    /**
+     * Callbacks управления ПЧ 'Дельта'
+     */
+    DELTA_FC_CONTROL_MENU,
+
+    // Курсовая
+    COURSEWORK_MENU,
+
+    // Дипломная
+    GRADUATEWORK_MENU,
+}
