aaf36fb86e929df987b766be9693cdab818b642f
Ruslan N <>
2023-03-12 00:47:26 +0300

    [MOD-6] optimized message sending and keyboard generation

--- src/main/java/bstu/graduate/modbus/command/CommandStart.java
+++ src/main/java/bstu/graduate/modbus/command/CommandStart.java
@@ -2,18 +2,15 @@ package bstu.graduate.modbus.command;
 
 import bstu.graduate.modbus.common.enums.CallbackEnum;
 import bstu.graduate.modbus.utils.GenerateInlineKeyboardMarkup;
+import bstu.graduate.modbus.utils.MessageBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.methods.ParseMode;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 
 @Component
 @RequiredArgsConstructor
-public class CommandStartImpl implements BaseCommand {
-    private final GenerateInlineKeyboardMarkup inlineKeyboardMarkup;
-
+public class CommandStart implements BaseCommand {
     @Override
     public BotCommand getBotCommand() {
         return BotCommand.builder()
@@ -25,14 +22,11 @@ public class CommandStartImpl implements BaseCommand {
     @Override
     public BotApiMethod<?> getAction(Long chatId) {
         int countButtons = 2;
+        String text = "Меню главного управления";
+
         String[] textButtons = {"Управление реле", "Управление ПЧ 'Дельта'"};
         CallbackEnum[] callbacks = {CallbackEnum.RELAY_CONTROL_MENU, CallbackEnum.DELTA_FC_CONTROL_MENU};
 
-        return SendMessage.builder()
-                .chatId(chatId)
-                .text("Меню главного управления")
-                .parseMode(ParseMode.MARKDOWN)
-                .replyMarkup(GenerateInlineKeyboardMarkup.withoutRow(countButtons, textButtons, callbacks))
-                .build();
+        return MessageBuilder.sendMessage(chatId, text, GenerateInlineKeyboardMarkup.withoutRow(countButtons, textButtons, callbacks));
     }
 }
