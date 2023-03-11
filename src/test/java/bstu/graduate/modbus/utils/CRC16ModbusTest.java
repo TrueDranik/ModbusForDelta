7a708267f811640fe829afd8aab2dbfdd2f0e59b
Ruslan N <>
2023-03-12 00:07:25 +0300

    [MOD-3] added methods for calculating the checksum

---
+++ src/test/java/bstu/graduate/modbus/utils/CRC16ModbusTest.java
@@ -0,0 +1,28 @@
+package bstu.graduate.modbus.utils;
+
+import bstu.graduate.modbus.common.enums.CommandForDelta;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CRC16ModbusTest {
+    @Test
+    void testSendBuffer() {
+        byte[] firstResult = {1, 6, 32, 0, 0, 18, 2, 7};
+        byte[] firstSendBuffer = CRC16Modbus.getSendBuffer(CommandForDelta.RUN_FORWARD.getBytes());
+        assertArrayEquals(firstResult, firstSendBuffer);
+
+        byte[] secondResult = {1, 5, 0, 0, -1, 0, -116, 58};
+        byte[] secondSendBuffer = CRC16Modbus.getSendBuffer(CommandForDelta.TURN_ON.getBytes());
+        assertArrayEquals(secondResult, secondSendBuffer);
+    }
+
+    @Test
+    void testBufferHexString() {
+        byte[] firstSendBuffer = {1, 6, 32, 0, 0, 18};
+        String bufferHexString = CRC16Modbus.getBufferHexString(firstSendBuffer);
+        String result = CommandForDelta.RUN_FORWARD.getBytes();
+        assertEquals(result, bufferHexString);
+    }
+}
