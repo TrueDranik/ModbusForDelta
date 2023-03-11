b1d0094e568eeb9a627b8acbdaab212aa3b093dc
Ruslan N <>
2023-03-12 01:22:43 +0300

    [MOD-6] added middleware method

--- src/main/java/bstu/graduate/modbus/service/callbackquery/Callback.java
+++ src/main/java/bstu/graduate/modbus/service/callbackquery/Callback.java
@@ -4,12 +4,19 @@ import bstu.graduate.modbus.common.enums.CallbackEnum;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
 
 import java.util.List;
 
 @Component
 public interface Callback {
-    List<BotApiMethod<?>> getCallbackQuery(CallbackQuery callbackQuery);
+    default List<BotApiMethod<?>> getCallbackQueries(CallbackQuery callbackQuery) {
+        Message message = callbackQuery.getMessage();
+
+        return apiMethodProcessor(message);
+    }
+
+    List<BotApiMethod<?>> apiMethodProcessor(Message message);
 
     CallbackEnum getActionCallback();
 }
