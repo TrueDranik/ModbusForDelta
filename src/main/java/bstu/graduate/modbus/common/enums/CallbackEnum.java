1dc190a514e8374c0f973e51ad2ec97609c632fa
Ruslan N <>
2023-04-09 12:56:20 +0300

    updated callbacks

--- src/main/java/bstu/graduate/modbus/common/enums/CallbackEnum.java
+++ src/main/java/bstu/graduate/modbus/common/enums/CallbackEnum.java
@@ -4,21 +4,12 @@ public enum CallbackEnum {
     START_MENU,
 
     /**
-     * Callbacks управления реле
-     */
-    RELAY_CONTROL_MENU,
-    TURN_ON_FIRST_RELAY,
-    TURN_OFF_FIRST_RELAY,
-
-    /**
      * Callbacks управления ПЧ 'Дельта'
      */
-    DELTA_FC_CONTROL_MENU,
+    CONTROL_COMMAND,
+    READING_STATUS_COMMAND,
 
-    // Курсовая
-    COURSEWORK_MENU,
-    START_COURSEWORK,
-
-    // Дипломная
-    GRADUATEWORK_MENU,
+    RUN_FORWARD,
+    RUN_REVERS,
+    STOP,
 }
