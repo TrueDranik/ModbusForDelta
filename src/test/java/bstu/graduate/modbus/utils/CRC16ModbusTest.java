16592f50761bfea4cc769e466f0bc8fcc27dcec3
Ruslan N <>
2023-04-09 18:55:11 +0300

    updated method

--- src/test/java/bstu/graduate/modbus/utils/CRC16ModbusTest.java
+++ src/test/java/bstu/graduate/modbus/utils/CRC16ModbusTest.java
@@ -10,11 +10,11 @@ class CRC16ModbusTest {
     @Test
     void testSendBuffer() {
         byte[] firstResult = {1, 6, 32, 0, 0, 18, 2, 7};
-        byte[] firstSendBuffer = CRC16Modbus.getSendBuffer(CommandForDelta.RUN_FORWARD.getBytes());
+        byte[] firstSendBuffer = CRC16Modbus.getSendBuffer(CommandForDelta.RUN_FORWARD.getByteArray());
         assertArrayEquals(firstResult, firstSendBuffer);
 
         byte[] secondResult = {1, 5, 0, 0, -1, 0, -116, 58};
-        byte[] secondSendBuffer = CRC16Modbus.getSendBuffer(CommandForDelta.TURN_ON.getBytes());
+        byte[] secondSendBuffer = CRC16Modbus.getSendBuffer(CommandForDelta.STOP.getByteArray());
         assertArrayEquals(secondResult, secondSendBuffer);
     }
 
@@ -22,7 +22,7 @@ class CRC16ModbusTest {
     void testBufferHexString() {
         byte[] firstSendBuffer = {1, 6, 32, 0, 0, 18};
         String bufferHexString = CRC16Modbus.getBufferHexString(firstSendBuffer);
-        String result = CommandForDelta.RUN_FORWARD.getBytes();
+        String result = CommandForDelta.RUN_FORWARD.getByteArray();
         assertEquals(result, bufferHexString);
     }
 }
