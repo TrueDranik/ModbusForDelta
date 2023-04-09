637247fcf763dc4f91d54a07a5f29b3c89834cbe
Ruslan N <>
2023-04-09 12:57:01 +0300

    added new command; edited constructor

--- src/main/java/bstu/graduate/modbus/common/enums/CommandForDelta.java
+++ src/main/java/bstu/graduate/modbus/common/enums/CommandForDelta.java
@@ -1,29 +1,49 @@
 package bstu.graduate.modbus.common.enums;
 
-import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public enum CommandForDelta {
-    @Deprecated
-    TURN_ON("01050000FF00"),
-    @Deprecated
-    TURN_OFF("010500000000"),
-    /**
-     * Запуск двигателя. Биты такие-то...
-     */
-    RUN_FORWARD("010620000012"),
 
     /**
-     * Остановить двигатель. Биты такие-то...
+     * Запуск двигателя вперед.
+     * Адрес в сети - 01; Функция - 06; Адрес регистра - 20 00; Команда управления - 00 12
      */
-    STOP("010620000001"),
+    RUN_FORWARD("01", CommandCode.WRITE, "2000", "0012"),
 
-    SET_FREQUENCY("01062000%s"),
-    GET_FREQUENCY("01062000%s");
+    RUN_REVERS("01", CommandCode.WRITE, "2000", "0022"),
 
+    STOP("01", CommandCode.WRITE, "2000", "0001"),
 
-    @Getter
-    private final String bytes;
-    CommandForDelta(String bytes) {
-        this.bytes = bytes;
+    SET_FREQUENCY("01", CommandCode.WRITE, "2001", "%s"),
+
+    GET_FREQUENCY("01", CommandCode.READ, "2103", "0001"),
+
+    SET_ACCELERATION_TIME("01", CommandCode.WRITE, "0109", "%s"),
+
+    SET_BRAKING_TIME("01", CommandCode.WRITE, "010A", "%s"),
+
+    GET_AMPERAGE("01", CommandCode.READ, "2104", "0001"),
+
+    GET_TEMP("01", CommandCode.READ, "2206", "0001"),
+
+    STOP_AFTER_SIGNAL_LOSS("01", CommandCode.WRITE, "0902", "0001"),
+
+    SET_RUNTIME_AFTER_SIGNAL_LOSS("01", CommandCode.WRITE, "0903", "%s"),
+
+    GET_OUTPUT_POWER("01", CommandCode.READ, "210F", "0001");
+
+    private final String networkAddress;
+    private final String commandCode;
+    private final String commandAddress;
+    private final String data;
+
+    public String getByteArray() {
+        return networkAddress + commandCode + commandAddress + data;
+    }
+
+    private static class CommandCode {
+        public static final String WRITE = "06";
+        public static final String READ = "03";
     }
 }
